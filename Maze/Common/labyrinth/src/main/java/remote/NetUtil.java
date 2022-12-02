3396d2ea8000511ed08ae47fd78b155713b3e393
tsetoguchi <setoguchi.t@northeastern.edu>
2022-12-01 19:09:05 -0500

    debugging netutil readnewinput

--- Maze/Common/labyrinth/src/main/java/remote/NetUtil.java
+++ Maze/Common/labyrinth/src/main/java/remote/NetUtil.java
@@ -3,18 +3,20 @@ package remote;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class NetUtil {
 
   public static void readNewInput(StringBuilder str, DataInputStream in) {
+    Scanner scanner = new Scanner(in);
     try {
       System.out.println("begin");
-      if(in.){
+      if (scanner.hasNext()) {
         String toAdd = in.readUTF();
         str.append(toAdd);
         System.out.println("end");
-
       }
+      scanner.close();
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
