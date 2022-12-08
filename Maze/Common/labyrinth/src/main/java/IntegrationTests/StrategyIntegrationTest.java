d0058bfb428cdad185134a56cd8532b6c73154ea
kwilmanja <kwilman.j@northeastern.edu>
2022-12-08 13:29:23 -0500

    Fix Strategy integration testing

---
+++ Maze/Common/labyrinth/src/main/java/IntegrationTests/StrategyIntegrationTest.java
@@ -0,0 +1,44 @@
+package IntegrationTests;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+
+import json.JsonDeserializer;
+import model.Position;
+import model.state.IState;
+import model.state.StateProjection;
+import player.EuclideanStrategy;
+import player.IPlayer;
+import player.IStrategy;
+import player.RiemannStrategy;
+
+
+public class StrategyIntegrationTest {
+    public static void main(String[] args) throws JSONException {
+
+      JSONTokener jsonTokener = IntegrationUtils.getInput();
+
+
+      String strategy = (String) jsonTokener.nextValue();
+      IStrategy s;
+      if(strategy.equals("Riemann")){
+        s = new RiemannStrategy();
+      } else{
+        s = new EuclideanStrategy();
+      }
+
+      JSONObject stateJSON = (JSONObject) jsonTokener.nextValue();
+      IState state = JsonDeserializer.state(stateJSON);
+      StateProjection sp = state.getStateProjection();
+
+      JSONObject goalJSON = (JSONObject) jsonTokener.nextValue();
+      Position goal = JsonDeserializer.coordinate(goalJSON);
+
+      System.out.println(s.createTurn(sp, goal));
+
+
+    }
+}
